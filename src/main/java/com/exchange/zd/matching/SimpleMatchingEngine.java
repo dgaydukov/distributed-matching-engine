b0142386c0a0b2a679d59ffbcd3f8d1f55642270
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-01-21 21:00:13 +0300

    add constructor

--- src/main/java/com/exchange/zd/matching/SimpleMatchingEngine.java
+++ src/main/java/com/exchange/zd/matching/SimpleMatchingEngine.java
@@ -1,6 +1,18 @@
 package com.exchange.zd.matching;
 
+import com.exchange.zd.kafka.MessageHandler;
+import com.exchange.zd.zookeeper.CoordinationHandler;
+
 public class SimpleMatchingEngine implements MatchingEngine {
+    private final MessageHandler messageHandler;
+    private final CoordinationHandler coordinationHandler;
+
+    public SimpleMatchingEngine(MessageHandler messageHandler,
+        CoordinationHandler coordinationHandler){
+        this.messageHandler = messageHandler;
+        this.coordinationHandler = coordinationHandler;
+    }
+
     @Override
     public void start() {
 
