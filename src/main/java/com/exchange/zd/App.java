0e18d96b14aaf03edb0945fd7adafe39078b7ebb
Dmitriy Gaydukov <gaydukov89@gmail.com>
2025-01-23 09:12:00 +0300

    add logger config

--- src/main/java/com/exchange/zd/App.java
+++ src/main/java/com/exchange/zd/App.java
@@ -7,9 +7,14 @@ import com.exchange.zd.matching.SimpleMatchingEngine;
 import com.exchange.zd.zookeeper.CoordinationHandler;
 import com.exchange.zd.zookeeper.ZookeeperCoordinationHandler;
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class App {
     public static void main(String[] args) {
+        System.out.println("start");
+        log.info("info");
+        log.debug("debug");
         emulateLeaderSelection();
 
 //        MessageHandler messageHandler = new KafkaMessageHandler("localhost:9092", "me-input", "me-output");
